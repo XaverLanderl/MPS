2a7907dbfc11686ebfe7fd7eeee3d176192f5c30
XaverLanderl <landerlxaver@gmail.com>
2024-11-03 19:27:41 +0100

    cleaned up and added useful functions

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from MPS_lib import *
 
 # parameters
-L = 50
+L = 51
 chi = 2*int(np.log(L) + 1)
 tau = 0.1
 J_z = 1.0
@@ -13,6 +13,7 @@ trunc_tol = 1
 list_of_spins_down = [int(L/2), int(L/2+1)]
 
 # initialize solver
-solver = MPS_solver(L=L, chi=chi, tau=tau, J_z=J_z, J_xy=J_xy, trunc_tol=trunc_tol, show_disc_weights=True)
+solver = MPS_solver(L=L, chi=chi, tau=tau, J_z=J_z, J_xy=J_xy, trunc_tol=trunc_tol)
 
-results = solver.run([int(L/4), int(3*L/4)], t_max=3*L)
+results = solver.run([int(L/4), int(3*L/4)])
+results2 = solver.run([int(L/2+1)])
